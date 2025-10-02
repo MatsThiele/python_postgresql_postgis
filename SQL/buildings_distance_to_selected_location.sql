e37c29b6828b9caa69ca2808c2746b76124b6908
MatsThiele <92586281+MatsThiele@users.noreply.github.com>
2025-10-02 09:18:13 +0000

    Prepare AP03

--- SQL/buildings_distance_to_selected_location.sql
+++ SQL/buildings_distance_to_selected_location.sql
@@ -18,3 +18,18 @@ WHERE
 p."addr:street" IS NOT NULL
 AND p."addr:city" = 'Zürich'
 AND p."addr:postcode" IN ('8055', '8003')
+
+SELECT
+            p.osm_id,
+            p.shop,
+            p.name
+            ST_Distance(
+                ST_Transform(p.way, 4326)::geography,
+                ST_SetSRID(ST_MakePoint(8.53936, 47.3781), 4326)::geography
+            ) AS distance_meters,
+            ST_TRANSFORM(p.way, 4326) AS geom
+         FROM
+            public.planet_osm_point AS p
+         WHERE
+            p.shop = 'hairdresser'
+            -- AND p.distance_meters <= 500m
